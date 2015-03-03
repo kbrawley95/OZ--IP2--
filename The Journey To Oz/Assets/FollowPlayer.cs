bb5a44a8a6fe88975fe15a717d7b5b8a118539a4
kbrawley95 <kieran.brawley95@gmail.com>
2015-03-03 21:17:10 +0000

    Further Wisp & Player Modifications

---
+++ The Journey To Oz/Assets/FollowPlayer.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FollowPlayer : MonoBehaviour {
+
+    public GameObject player;
+    public float speed = 1f;
+    
+
+	// Use this for initialization
+	void Start () {
+	    
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        Vector3 direction = (player.transform.position - transform.position).normalized;
+        GetComponent<Rigidbody2D>().velocity = direction * speed * Time.deltaTime;
+
+        if(transform.position==player.transform.position)
+        {
+            speed = 0;
+        }
+
+	}
+}
