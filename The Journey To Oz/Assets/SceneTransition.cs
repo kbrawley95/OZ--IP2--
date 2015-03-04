5f4d9372e0b2a777c9ad61849758eb60efad4cf1
kbrawley95 <kieran.brawley95@gmail.com>
2015-03-04 13:08:14 +0000

    Latest Build

---
+++ The Journey To Oz/Assets/SceneTransition.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SceneTransition : MonoBehaviour {
+
+    public string scene;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    void OnTriggerEnter2D(Collider2D target)
+    {
+        if (target.gameObject.tag == "Player")
+        {
+            Application.LoadLevel(scene);
+        }
+    }
+}
