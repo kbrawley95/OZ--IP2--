df52bb998435cab6dbad55af1ac9f5a399b7af61
HarryMcMahon <harry.mcmahon@hotmail.co.uk>
2015-03-05 13:42:34 +0000

    WeaponSelectionScript
    Sxc scripts, uploading is ard

---
+++ The Journey To Oz/Assets/Scripts/Weapons.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Weapons : MonoBehaviour {
+
+	public GameObject[] weapons; // add weapons in editor drag and drop
+	public int currentWeapon = 0;
+	public int numWeapons;
+	// Use this for initialization
+	void Start () {
+	
+		numWeapons = weapons.Length;
+		changeWeapon(currentWeapon); // sets default (unarmed??)
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+		if(Input.GetKeyDown (KeyCode.Alpha1)){
+			changeWeapon(0);
+		}
+
+			if(Input.GetKeyDown (KeyCode.Alpha2)){
+				changeWeapon(1);
+	}
+	}
+
+     public void changeWeapon(int num){
+			currentWeapon = num;
+				for(int i = 0; i < numWeapons; i++)
+					if(i == num)
+						weapons[i].gameObject.SetActive (true);
+				else
+					weapons[i].gameObject.SetActive (false);
+
+
+}
+}
