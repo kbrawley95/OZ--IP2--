e267f2affc8912a3d2d1b385629603931bcc94e1
kbrawley95 <kieran.brawley95@gmail.com>
2015-03-19 21:14:53 +0000

    Revert "Refined Pause Menu System"
    This reverts commit 326ba85f6a78c96f2df664e4cc0867a449f83808.

--- The Journey To Oz/Assets/Scripts/PauseMenu.cs
+++ The Journey To Oz/Assets/Scripts/PauseMenu.cs
@@ -4,7 +4,6 @@ using System.Collections;
 public class PauseMenu : MonoBehaviour {
 
      public bool paused = false;
-        
  
      void Update()
      {
@@ -16,9 +15,8 @@ public class PauseMenu : MonoBehaviour {
      {
          if(paused)
          {
-             GUILayout.BeginArea (new Rect((Screen.width/2)-50, (Screen.height/2), 100, 100), "<color=White><size=40>Paused</size></color>");
-             //GUILayout.Label("Game is paused!");
-             
+             GUILayout.BeginArea(new Rect(750, 300, 200, 200));
+             GUILayout.Label("Game is paused!");
              GUILayout.EndArea();
 
             
